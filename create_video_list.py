6045c54557d211cc0d5cec884c7882c23c8b30d7
sayumujina_ <76656258+sayumujina@users.noreply.github.com>
2024-08-12 01:15:51 +0700

    Update create_video_list.py

--- create_video_list.py
+++ create_video_list.py
@@ -7,7 +7,6 @@ import video_library as lib
 import font_manager as fonts
 import csv
 
-#MISC Functions
 def set_text(text_area, content):
     text_area.delete("1.0", END)
     text_area.insert(1.0, content)
@@ -19,51 +18,55 @@ def errorID():
     messagebox.showwarning(title="Invlid ID", message="Please enter a valid ID")
 def errorDUP():
     messagebox.showinfo(title="Duplicate found", message="The video has already been added")
+
+#___________GUI_____________#
 class CreateVideolist():
         def __init__(self,window):
-            window.geometry("1080x510")
+            window.geometry("1140x580")
             window.title("Create Your Playlist")
-            window.minsize(1080,510)
-            window.maxsize(1080,510)
+            window.minsize(1140,580)
+            window.maxsize(1140,580)
             self.videoplaylist=[]
 
-            listall_button= Button(window, text="List All Videos",command = self.listall)
-            listall_button.grid(row=0, column=0, padx=10, pady=10)
+            self.list_txt = tkst.ScrolledText(window, width=60, height=9, wrap="none")
+            self.list_txt.place(x=10, y=10)
+
+            list_videos_btn= tk.Button(window, text="List All Videos", command=self.list_videos_clicked)
+            list_videos_btn.place(x=13, y=228)
+
+            self.search_input = Entry(window, width=21)
+            self.search_input.place(x=230, y=295)
+        
+            search_lbl = Label(window,text="Enter search term")
+            search_lbl.place(x=10, y=290)
+                    
+            self.search_button = Button(window, text="Search", command=self.search_clicked)
+            self.search_button.place(x=485, y=290)
             
-            self.list_txt = tkst.ScrolledText(window, width=48, height=12, wrap="none")
-            self.list_txt.grid(row=1, column=0, columnspan=3, sticky="W", padx=10, pady=10)
+            self.Video_ID = Label(window,text="Enter video number")
+            self.Video_ID.place(x=10, y=353)
             
-            self.Video_ID = Label(window,text="Enter Video ID")
-            self.Video_ID.grid(row=0, column=1, padx=10, pady=10)
-            
-            self.ID_input = Entry(window, width=3)
-            self.ID_input.grid(row=0, column=2, padx=8, pady=10)
+            self.ID_input = Entry(window, width=17)
+            self.ID_input.place(x=245, y=358)
             
             self.add_video = Button(window,text="Add Video",command=self.add_btn_clicked)
-            self.add_video.grid(row=0, column=3, padx=8, pady=10)
+            self.add_video.place(x=449, y=350)
             
             self.delete_list = Button(window,text="Clear list",command=self.clear_btn_clicked)
-            self.delete_list.grid(row=0, column=4, padx=8, pady=10)
+            self.delete_list.place(x=466, y=410)
             
-            self.playlist = tkst.ScrolledText(window, width=20, height=12, wrap="none")
-            self.playlist.grid(row=1, column=3, columnspan=3, sticky="W", padx=10, pady=10)
+            self.playlist = tkst.ScrolledText(window, width=18, height=9, wrap="none")
+            self.playlist.place(x=598, y=290)
 
             self.play_btn = Button(window,text="Play",command=self.play_btn_clicked)
-            self.play_btn.grid(row=2, column=3, padx=8, pady=10)
-            
-            search_lbl = Label(window,text="Enter parameters")
-            search_lbl.grid(row=2, column=0, padx=10, pady=10)
-                    
-            self.search_input = Entry(window, width=30)
-            self.search_input.grid(row=2, column=1, padx=10, pady=10, sticky='E')
+            self.play_btn.place(x=515, y=470)
 
             self.status_lbl = tk.Label(window, text="", font=("Helvetica", 10))
-            self.status_lbl.grid(row=3, column=0, columnspan=4, sticky="W", padx=10, pady=10)
+            self.status_lbl.place(x=900, y=10)
 
-            self.search_button = Button(window, text="Search", command=self.search_clicked)
-            self.search_button.grid(row=2, column=2, columnspan=5, sticky='W')
+            self.list_videos_clicked() 
 
-        # buttons
+        #___________search function_____________#
         def search_clicked(self):
             library = []
             with open('info.csv') as csv_file:
@@ -99,12 +102,12 @@ class CreateVideolist():
                 set_text(self.list_txt, video_details)
 
             self.status_lbl.configure(text="Search button was clicked!")
-
+        #___________commands for clicking their corresponding button_____________#
         def play_btn_clicked(self):
             id = self.ID_input.get()
             play_count = lib.get_play_count(id)
-            
             play_count += 1
+            self.status_lbl.configure(text="Play button was clicked!")
 
         def add_btn_clicked(self):
             key = self.ID_input.get()
@@ -120,14 +123,17 @@ class CreateVideolist():
                     errorID()
             elif x in self.videoplaylist:
                 errorDUP()
+            self.status_lbl.configure(text="Add video button was clicked!")
                 
         def clear_btn_clicked(self):
             self.playlist.delete("1.0", "end")
             self.videoplaylist.clear()
-        
-        def listall(self):
-            showlist = lib.list_all()
-            set_text(self.video_box,showlist)
+            self.status_lbl.configure(text="Clear list button was clicked!")
+
+        def list_videos_clicked(self):
+            video_list = lib.list_all()
+            set_text(self.list_txt, video_list)
+            self.status_lbl.configure(text="List Videos button was clicked!")
         
 if __name__== "__main__":
     window = Tk()
